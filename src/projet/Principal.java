e4d7af5d4a0e68879e20830675ab126d3e481d60
Antoine La Boissiere <antoine.laboissiere@gmail.com>
2020-02-17 09:32:12 -0500

    For

--- src/projet/Principal.java
+++ src/projet/Principal.java
@@ -7,19 +7,30 @@ public class Principal {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader lecteurAvecBuffer = null;
+		String[] fichier = new String[12];
         String ligne;
+        int nb = 0;
 
-        try
-          {
-        lecteurAvecBuffer = new BufferedReader(new FileReader("test.txt"));
-          }
-        catch(FileNotFoundException exc)
-          {
-        System.out.println("Erreur d'ouverture");
-          }
-        while ((ligne = lecteurAvecBuffer.readLine()) != null)
-          System.out.println(ligne);
-        lecteurAvecBuffer.close();
+        try {
+        	
+        	lecteurAvecBuffer = new BufferedReader(new FileReader("test.txt"));
+        
+        }
+        
+        catch(FileNotFoundException exc) {
+        	
+        	System.out.println("Erreur d'ouverture");
+        
+        }
+        
+        for (int i = 0; lecteurAvecBuffer.readLine() != null; i++) {
+        	
+        	ligne = lecteurAvecBuffer.readLine();
+        	System.out.println(ligne);
+        	
+        }
+        
+        	lecteurAvecBuffer.close();
+        	
       }
-
 }
