4ff49fdebbda19262e146fc21bb564b7814d150e
Michant-bit <60196454+Michant-bit@users.noreply.github.com>
2020-02-23 19:21:14 -0500

    Ajout de Client.java

---
+++ src/projet/Client.java
@@ -0,0 +1,29 @@
+package projet;
+
+import java.text.DecimalFormat;
+
+public class Client {
+	
+	private static DecimalFormat df = new DecimalFormat("0.00");
+	
+	String nom;
+	float prix;
+	
+	public Client() {
+		
+	}
+	
+	public Client(String nom, float prix) {
+		
+		this.nom = nom;
+		this.prix += prix;
+		
+	}
+	
+	public String afficher() {
+		
+		return nom + " " + df.format(prix) + " $\n";
+		
+	}
+
+}
